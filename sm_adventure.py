1b8c1ab049e32a8be67b1fb86984d8e574ae1e8e
EyeOfPython <ruck.tobias@gmail.com>
2014-05-23 00:47:57 +0200

    Added sm_adventure.py and swapped order of jobs in sm_build.py

---
+++ sm_adventure.py
@@ -0,0 +1,18 @@
+'''
+Created on 23.05.2014
+
+@author: Tobias Ruck
+'''
+from state import StateMachine
+
+class StateMachineAdventure(metaclass=StateMachine):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
